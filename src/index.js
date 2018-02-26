b177f10fa4fcf94525e25917c14e70d097b0d2d7
antonpalinau <antonpalinau@gmail.com>
2018-02-26 18:55:13 +0300

    solved sorter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+    this.compareFunction = (a,b) => {return a-b};
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return this.arr;
   }
 
   sort(indices) {
     // your implementation
+    let arrToSort = [];
+    indices.sort((a,b) => {return a-b});
+    for(let i = 0; i < indices.length; i++) {
+      arrToSort.push(this.arr[indices[i]]);
+    }
+    arrToSort.sort(this.compareFunction);
+    for(let i = 0; i < indices.length; i++) {
+      this.arr[indices[i]]=arrToSort[i];
+      
+    }
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
+
 }
 
 module.exports = Sorter;
